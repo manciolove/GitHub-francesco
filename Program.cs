c05c6e3108f710b9c33a50d8a25193df4dafe714
manciolove <114284283+manciolove@users.noreply.github.com>
2025-10-23 10:08:48 +0200

    Aggiungere i file di progetto.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace GitHub_francesco
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
